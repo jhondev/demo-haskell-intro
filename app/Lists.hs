2ef017a7a99012bfb6f382cbf6a9c57da054330f
jhondev <jma9974@gmail.com>
2022-01-07 17:13:22 -0500

    lists exercises

--- app/Lists.hs
+++ app/Lists.hs
@@ -6,26 +6,47 @@ exercises = do
   let list = [1 .. 5]
   print list
 
-  print "head"
+  putStrLn "\nhead"
   print (head list)
 
-  print "tail"
+  putStrLn "\ntail"
   print (tail list)
 
-  print "nth"
+  putStrLn "\nnth"
   print (list !! 2)
 
-  print "first 3 elements"
+  putStrLn "\nfirst 3 elements"
   print (take 3 list)
 
-  print "drop first 3 elements"
+  putStrLn "\ndrop first 3 elements"
   print (drop 3 list)
 
-  print "sum"
+  putStrLn "\nsum"
   print (sum list)
 
-  print "pow3List"
+  putStrLn "\npow3List"
   print ([3 ^ n | n <- list])
 
-  print "append 2 list"
+  putStrLn "\nappend 2 list"
   print (list ++ [8, 9])
+
+  putStrLn "\nlist comprehensions powList"
+  let powList = [x ^ 2 | x <- list]
+  print powList
+
+  putStrLn "\nlist comprehensions with dependant generators doublePowList"
+  let doublePowList = [(x ^ 2, y ^ 2) | x <- list, y <- [x, 20, 30]]
+  print doublePowList
+
+  putStrLn "\nlist comprehensions with guards"
+  let guardFunc = [x | x <- [1 .. 10], even x]
+  print guardFunc
+
+  putStrLn "\nzip list"
+  let zipped = zip list ['a', 'b', 'c']
+  print zipped
+
+  let pairs xs = zip xs (tail xs)
+  putStrLn "\nis list sorted?"
+  let sorted xs = and [x <= y | (x, y) <- pairs xs]
+  print (sorted list)
