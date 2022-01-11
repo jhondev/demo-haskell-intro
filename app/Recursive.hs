0e43c842d28bfe2b3a8dc81538c2c5339a6dd929
jhondev <jma9974@gmail.com>
2022-01-11 11:46:29 -0500

    TODOS in recursion

---
+++ app/Recursive.hs
@@ -0,0 +1,18 @@
+--
+
+{-- TODO:
+FP 8-9 - Recursive functions
+- decide if all logical values in a list are true: and :: [Bool] -> Bool
+- concatenate a list of lists: concat :: [[a]] -> [a]
+- produce a list with n identical elements: replicate :: Int -> a -> [a]
+- select the nth element of a list: (!!) :: [a] -> Int -> a
+- decide if a value is an element of a list: elem :: Eq a => a -> [a] -> Bool
+- Merge 2 sorted lists of values to give a single sorted list: merge :: Ord a => [a] -> [a] -> [a]
+- quicksort using recursion
+
+- how foldr howks?
+
+FP 10 exercises
+
+FP 11 How to think recursively
+--}
